d1faec5fbde0e426db0ef908472b9574f882bc37
griffin-schumock <schumockg@gmail.com>
2022-05-11 00:29:40 -0700

    GriffinAdd Scene
    Includes play again button and a new font

--- Assets/Scripts/HUDscript.cs
+++ Assets/Scripts/HUDscript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class HUDscript : MonoBehaviour
@@ -9,28 +10,41 @@ public class HUDscript : MonoBehaviour
     public static int lives;
 
     public GameObject gameOverText;
+    public GameObject playAgainButton;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        Time.timeScale = 1;
         lives = 1;
         SetLivesText();
         gameOverText.SetActive(false);
+        playAgainButton.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //this could be better. would call SetLivesText() after a cat dies but not sure how to call inside the CatAI script
         SetLivesText();
         if (lives == 0)
         {
             gameOverText.SetActive(true);
+            playAgainButton.SetActive(true);
             Time.timeScale = 0;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
         }
     }
 
     public void SetLivesText()
     {
         livesText.text = "Lives: " + lives.ToString();
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
