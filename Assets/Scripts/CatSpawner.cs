ebb38b26949b3810f2eeca83f5643c44f7e3f683
Austin-Mello <60442413+Austin-Mello@users.noreply.github.com>
2022-05-10 18:41:54 -0700

    Added comments

--- Assets/Scripts/CatSpawner.cs
+++ Assets/Scripts/CatSpawner.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class CatSpawner : MonoBehaviour
 {
+    //Creates all the objects the cat will interact with.
     public List<Transform> randomPositions;
     public List<Transform> goalPositions;
     public Hazard hazard;
 
+    //The spawn rate.
     private const float SPAWN_RATE = 30;
 
+    //The Cat prefab.
     public CatAI catPrefab;
 
+    //Instantiates the cat prefab, and gives it the objects it will interact with.
     public void spawnCat()
     {
         CatAI catAI = Instantiate(catPrefab, transform.position, Quaternion.identity);
@@ -19,11 +23,15 @@ public class CatSpawner : MonoBehaviour
         catAI.setPositions(randomPositions, goalPositions, hazard);
     }
 
+    //Calls the spawning loop at startup.
     private void Start()
     {
         StartCoroutine("spawnTimer");
     }
 
+    //At an interval of SPAWN_RATE, this loop will spawn a new cat prefab!
+    //At this time, there is no breakout case.  There probly won't be one,
+    // since it's a survival game ;)
     private IEnumerator spawnTimer()
     {
         while (true)
--- Assets/Scripts/Hazard.cs
+++ Assets/Scripts/Hazard.cs
@@ -4,22 +4,33 @@ using UnityEngine;
 
 public class Hazard : MonoBehaviour
 {
+    //A 'trapped' boolean to notify the player object(s) that the hazard object
+    // can be interacted with.
     public bool trapped = false;
+    
+    //An 'occupied' boolean for the cat objects so they don't end up dog-piling
+    // (pun intended) the hazard.
     public bool occupied = false;
+    
+    //A cat object.  To be defined in TrapCat()
     public CatAI Cat;
 
-    // Start is called before the first frame update
+    //Since the hazard is totally reactionary, there's not really anything going
+    // on during startup.
     void Start()
     {
 
     }
 
-    // Update is called once per frame
+    //Probly should've put something in here, too.. Everything seems to work though
+    // soooo...
     void Update()
     {
         
     }
 
+    //Calls the rescue function in the CatAI script and resets the cat object.
+    //Will change this to call the help() function once we get a health bar going.
     public void freeCat()
     {
         trapped = false;
@@ -28,25 +39,23 @@ public class Hazard : MonoBehaviour
         Debug.Log("The cat is free!");
     }
 
+    //Gets the CatAI component from the function call and stores it in the cat
+    // variable.  Then, set the cat's state to distressed.
     void TrapCat(GameObject other)
     {
         Cat = other.GetComponent(typeof(CatAI)) as CatAI;
         Debug.Log("The cat is trapped!");
         Cat.setState(CatState.DISTRESS);
         trapped = true;
-        
     }
 
+    //The trigger to detect a cat collision! Calls the TrapCat function and
+    // passes it the cat.
     private void OnTriggerEnter(Collider col)
     {
         if(col.gameObject.tag == "Cat")
         {
             TrapCat(col.gameObject);
         }
-        /*CatAI cat = col.getComponent<Cat>();
-        if (cat != null)
-        {
-            TrapCat(cat);
-        }*/
     }
 }
